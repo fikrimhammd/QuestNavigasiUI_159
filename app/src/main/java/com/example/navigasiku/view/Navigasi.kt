be5820eff570bc4db11c316467ed5df9615b0194
fikrimhammd <fikri.kdi60@gmail.com>
2025-11-07 13:00:49 +0700

    Menyelesaikan Navigasi.kt

--- app/src/main/java/com/example/navigasiku/view/Navigasi.kt
+++ app/src/main/java/com/example/navigasiku/view/Navigasi.kt
@@ -1,2 +1,49 @@
 package com.example.navigasiku.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp() {
+    val navController: NavHostController = rememberNavController()
+
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+            modifier = androidx.compose.ui.Modifier.padding(paddingValues = isiRuang)
+        ) {
+            composable(route = Navigasi.Formulirku.name) {
+                FormIsian(
+                    OnSubmitBtnClick = {
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
+
+            composable(route = Navigasi.Detail.name) {
+                TampilData(
+                    onBackBtnClick = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
+        }
+    }
+}
+
+private fun cancelAndBackToFormulir(
+    navController: NavHostController
+) {
+    navController.popBackStack(route = Navigasi.Formulirku.name, inclusive = false)
+}
